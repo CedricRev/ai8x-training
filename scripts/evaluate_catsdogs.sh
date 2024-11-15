66b55035c92bc5b0319f8d789816a73ae231c9a2
CedricRev <53487258+CedricRev@users.noreply.github.com>
2024-11-15 17:28:42 +0800

    Personal Scripts

--- scripts/evaluate_catsdogs.sh
+++ scripts/evaluate_catsdogs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --model ai85cdnet --dataset cats_vs_dogs --confusion --evaluate --exp-load-weights-from ../ai8x-synthesis/trained/ai85-catsdogs-qat8-q.pth.tar -8 --device MAX78000 "$@"
+python train.py --model ai85cdnet --dataset cats_vs_dogs --confusion --evaluate --exp-load-weights-from ../ai8x-training/logs/2024.11.15-151510/best_quantized.pth.tar -8 --device MAX78000 "$@"
